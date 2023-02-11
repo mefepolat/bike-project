dc5bb5347f7457515e4f31a083369e958cf68a19
Efe Polat <mefepolat@hotmail.com>
2023-02-11 00:20:13 -0500

    dnm

--- client/src/shared/components/Navbar.jsx
+++ client/src/shared/components/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css"
 const NavBar = () => {
     return (
         <nav>
-            <a href="#">Home</a>
+            <a href="/">Home</a>
             <a href="/about">ABOUT</a>
             <a href="/login">Sign in</a>
             <a href="/register">Sign up</a>
