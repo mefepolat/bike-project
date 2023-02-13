f0595c8414175dd94025c81b993fff4438822e85
muratirmak <mrtriver@gmail.com>
2023-02-13 09:53:32 -0500

    SelectStation one is removed

--- client/src/home/pages/Home.js
+++ client/src/home/pages/Home.js
@@ -1,14 +1,14 @@
-// import CheckOutButton from "../components/CheckOutButton";
 
-<<<<<<< HEAD
+
+
 import CheckOutButton from "../components/CheckOutButton";
-import SelectStation from "../components/SelectStation"
-=======
+
+
 import SelectStation from "../components/SelectStation";
 import {AuthContext } from "../../shared/components/AuthContext";
 import { useContext } from "react";
 
->>>>>>> 970cac71b2dd3a29c98c0f1626a93419121d2571
+
 
 
 const HomePage = () => {
