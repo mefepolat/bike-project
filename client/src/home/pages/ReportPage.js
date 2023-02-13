55bdee970a66b9fea45c1c63d8f61f8fb308d687
muratirmak <mrtriver@gmail.com>
2023-02-13 15:07:03 -0500

    Report page api handling

--- client/src/home/pages/ReportPage.js
+++ client/src/home/pages/ReportPage.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 
+
+
+
+
+
 const ReportPage = () => {
   const [report, setReport] = useState('');
+  console.log("Here is the report", report)
 
   const handleChange = (event) => {
     setReport(event.target.value);
@@ -9,6 +15,22 @@ const ReportPage = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    try {
+      const response = fetch('http://localhost:3000/api/report', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ report })
+      });
+      console.log(response);
+      // Add logic to handle successful sign in
+    } catch (error) { 
+      console.error(error);
+    } finally {
+      console.log("finally" + report)
+    }
+
 
     // Add code to submit the report here
   };
