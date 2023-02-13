0a9006c3a73baaa777a72276d78edc763f7ed06a
muratirmak <mrtriver@gmail.com>
2023-02-13 12:28:53 -0500

    Report button and report pages added

---
+++ client/src/home/components/ReportButton.js
@@ -0,0 +1,21 @@
+import './ReportButton.css';
+import { useNavigate } from 'react-router-dom';
+
+
+const ReportButton = () => {
+const navigate = useNavigate();
+
+    const handleClick = () => {
+        console.log("report button clicked")
+      navigate("/report");
+
+    }
+
+  return (
+    <button className="report_button" onClick={handleClick}>
+        Demage or loss report
+        </button>
+  );
+};
+
+export default ReportButton;
--- client/src/home/pages/Home.js
+++ client/src/home/pages/Home.js
@@ -2,27 +2,36 @@ import CheckOutButton from "../components/CheckOutButton";
 import SelectStation from "../components/SelectStation";
 import {AuthContext } from "../../shared/components/AuthContext";
 import { useContext } from "react";
+import ReportButton from "../components/ReportButton";
 
 
 
 
 const HomePage = () => {
-    const {user} = useContext(AuthContext);
-    console.log(user);
+    const { user } = useContext(AuthContext);
+  
     return (
+      <div className="home_section">
+        <h1>HomePage</h1>
+  
+        {user ? (
+           <div>
+           <SelectStation />
+          <CheckOutButton />
+          <ReportButton />
+            
+            </div>
         
-        <div className="home_section">
-            <h1>HomePage</h1>
-            
-            
-            <SelectStation />
-            <CheckOutButton/>
-
-          
-            
-        </div>
-        
-    )
-};
-
-export default HomePage;
+        ) : (
+          <div>
+            <a href="/login">Sign in</a>
+            <a href="/register">Sign up</a>
+          </div>
+        )}
+  
+       
+      </div>
+    );
+  };
+  
+  export default HomePage;
